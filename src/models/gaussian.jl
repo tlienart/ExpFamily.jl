c40623361186a36f65677ded05a5e9b2f4d3db06
Thibaut Lienart <ltib@me.com>
2017-05-22 18:06:22 +0100

    .

--- src/models/gaussian.jl
+++ src/models/gaussian.jl
@@ -28,24 +28,33 @@ end
 #     mu2::Vector{Float}
 # end
 
+# Short types to simplify internal code
+const GaussNP = GaussianNatParam
+const GaussMP = GaussianMeanParam
+const Gauss   = Union{GaussNP, GaussMP}
+
 #################
 ## Constructors #
 #################
 
-function GaussianNatParam( t1::Vector{Float}, t2::Matrix{Float}, check=false )
-    (check && !isposdef(-t2)) ? throw(DomainError) : nothing
+function GaussianNatParam(t1::Vector{Float}, t2::Matrix{Float},
+                          check=false)::GaussNP
+    (check && !isposdef(-t2)) ? throw(DomainError()) : nothing
     GaussianNatParam(t1, Symmetric(t2))
 end
 function GaussianNatParam(;mu::Vector{Float}=[0.0], cov::Matrix{Float}=[1.0],
-                           check = false)
+                           check=false)::GaussNP
     P = inv(cov)
     GaussianNatParam(P*mu, -P, check)
 end
 
-function GaussianMeanParam(m1::Vector{Float}, m2::Matrix{Float})
+function GaussianMeanParam(m1::Vector{Float}, m2::Matrix{Float},
+                           check=false)::GaussMP
+    (check && !isposdef(2m2-m1*m1')) ? throw(DomainError()) : nothing
     GaussianMeanParam(m1, Symmetric(m2))
 end
-function GaussianMeanParam(;mu::Vector{Float}=[0.0], cov::Matrix{Float}=[1.0])
+function GaussianMeanParam(;mu::Vector{Float}=[0.0], cov::Matrix{Float}=[1.0],
+                            check=false)::GaussMP
     GaussianMeanParam(mu, 0.5(mu*mu'+cov))
 end
 
@@ -53,11 +62,11 @@ end
 ## Conversion #
 ###############
 
-function natparam(g::GaussianMeanParam)::GaussianNatParam
+function natparam(g::GaussMP)::GaussNP
     P = inv(Symmetric(2.0g.mu2.data-g.mu1*g.mu1'))
     GaussianNatParam(P*g.mu1, -P)
 end
-function meanparam(g::GaussianNatParam)::GaussianMeanParam
+function meanparam(g::GaussNP)::GaussMP
     cov = -inv(g.theta2)
     mu1 = cov * g.theta1
     GaussianMeanParam(mu1, 0.5(mu1*mu1' + cov))
@@ -67,28 +76,28 @@ end
 ## Operations extending base #
 ##############################
 
-Base.mean(g::GaussianNatParam)     = -g.theta2\g.theta1
-Base.mean(g::GaussianMeanParam)    = g.mu1
-Base.cov(g::GaussianNatParam)      = -inv(g.theta2)
-Base.cov(g::GaussianMeanParam)     = 2.0g.mu2.data-g.mu1*g.mu1'
-Base.isvalid(g::GaussianNatParam)  = isposdef(-g.theta2.data)
-Base.isvalid(g::GaussianMeanParam) = isposdef(2.0g.mu2.data-g.mu1*g.mu1')
+Base.mean(g::GaussNP) = -g.theta2\g.theta1
+Base.mean(g::GaussMP) = g.mu1
+Base.cov(g::GaussNP)  = -inv(g.theta2)
+Base.cov(g::GaussMP)  = 2.0g.mu2.data-g.mu1*g.mu1'
+Base.var(g::Gauss)    = diag(cov(g))
 
-Base.ones(::GaussianNatParam, d::Int) =
-    GaussianNatParam(mu=zeros(d), cov=eye(d))
-Base.ones(::GaussianMeanParam, d::Int) =
-    GaussianMeanParam(mu=zeros(d), cov=eye(d))
+Base.isvalid(g::GaussNP)  = isposdef(-g.theta2.data)
+Base.isvalid(g::GaussMP) = isposdef(2.0g.mu2.data-g.mu1*g.mu1')
+
+Base.ones(::GaussNP, d::Int) = GaussianNatParam(mu=zeros(d), cov=eye(d))
+Base.ones(::GaussMP, d::Int) = GaussianMeanParam(mu=zeros(d), cov=eye(d))
 # zeros does not really make sense
 
 ###################
 ## Safe operators #
 ###################
 
-+{T<:GaussianNatParam}(g1::T, g2::T) =
++(g1::GaussNP, g2::GaussNP) =
     GaussianNatParam(g1.theta1+g2.theta1, g1.theta2+g2.theta2)
 
 ####################
 ## Unafe operators #
 ####################
 
-#-{T<:GaussianNatParam}(g1::GaussianNatParam)
+#-{T<:GaussianNatParam}(g1::GaussNP)
